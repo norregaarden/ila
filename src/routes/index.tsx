9e3d11d3e903df46e17955d91967618c0a2164ee
Johan Nørregaard Andersen <jna.jelling@gmail.com>
2023-05-17 23:52:19 +0200

    lol

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,28 @@
+import Lol from "./../../lia/lol";
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 
+// export function Lol(){
+//   return <h1>Lol component</h1>
+// }
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Lol />
     </main>
   );
 }
+
+const solidStartDefault =
+  <>
+    <Counter />
+    <p>
+      Visit{" "}
+      <a href="https://start.solidjs.com" target="_blank">
+        start.solidjs.com
+      </a>{" "}
+      to learn how to build SolidStart apps.
+    </p>
+  </>
